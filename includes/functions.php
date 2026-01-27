7798e59c9d0a26831541d01c59b49bd3f11d2452
deji98 <deji98.adesinafelix98@gmail.com>
2026-01-27 09:40:55 +0100

    If tiny_slug doesn't exist, generate and save one

--- includes/functions.php
+++ includes/functions.php
@@ -76,12 +76,12 @@ if ( ! function_exists( 'tinypress_get_ip_address' ) ) {
 	 */
 
 	function tinypress_get_ip_address() {
-		if ( isset( $_SERVER['HTTP_CLIENT_IP'] ) && ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+		if ( ! empty( sanitize_text_field( $_SERVER['HTTP_CLIENT_IP'] ) ) ) {
 			$ip = sanitize_text_field( $_SERVER['HTTP_CLIENT_IP'] );
-		} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) && ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+		} elseif ( ! empty( sanitize_text_field( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) ) {
 			$ip = sanitize_text_field( $_SERVER['HTTP_X_FORWARDED_FOR'] );
 		} else {
-			$ip = isset( $_SERVER['REMOTE_ADDR'] ) ? sanitize_text_field( $_SERVER['REMOTE_ADDR'] ) : '0.0.0.0';
+			$ip = sanitize_text_field( $_SERVER['REMOTE_ADDR'] );
 		}
 
 		return $ip;
@@ -238,7 +238,14 @@ if ( ! function_exists( 'tinypress_get_tinyurl' ) ) {
 		}
 
 		// added the tiny slug
-		$tinyurl_parts[] = Utils::get_meta( 'tiny_slug', $tinypress_link_id );
+		$tiny_slug = Utils::get_meta( 'tiny_slug', $tinypress_link_id );
+		
+		if ( empty( $tiny_slug ) ) {
+			$tiny_slug = tinypress_create_url_slug();
+			update_post_meta( $tinypress_link_id, 'tiny_slug', $tiny_slug );
+		}
+		
+		$tinyurl_parts[] = $tiny_slug;
 
 		return apply_filters( 'TINYPRESS/Filters/get_tinyurl', implode( '/', $tinyurl_parts ), $tinypress_link_id, $tinyurl_parts );
 	}
