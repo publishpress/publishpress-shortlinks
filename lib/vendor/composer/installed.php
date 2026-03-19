30b5a7a27aceaba12e261ae14461ce78eac88d13
Anderson Martins <anderson@publishpress.com>
2026-03-19 11:03:11 -0300

    Update composer.lock and lib/composer.lock

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'bcdf1c13952ef5111ec423f3f74b8b314b2537df',
+        'reference' => '918090d18a3186fcecf268a2b862a67ba98ea9f6',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'bcdf1c13952ef5111ec423f3f74b8b314b2537df',
+            'reference' => '918090d18a3186fcecf268a2b862a67ba98ea9f6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
