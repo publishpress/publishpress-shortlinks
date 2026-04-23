4ab9ebcf10f217556a4bfc923159fc0424061a44
Adedeji Adesina <adesinafelix98@gmail.com>
2026-04-23 15:51:55 +0100

    include bundled translations library

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '0f4107b750ea9a82c50cad85133cd7aa6a898352',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,11 +11,20 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '0f4107b750ea9a82c50cad85133cd7aa6a898352',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'publishpress/bundled-translations' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'e69381e8695fa8e6de3fc9c9d534a133ab856532',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../publishpress/bundled-translations',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
