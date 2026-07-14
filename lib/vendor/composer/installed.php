b2fda9aa4c1af9ab19a4d804dc7ee52c043a996f
Adedeji Adesina <adesinafelix98@gmail.com>
2026-07-14 08:56:03 +0100

    composer update

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'f71c16462b3150068c887297858b3c28d68ee588',
+        'reference' => '86fc527dc3d8f97dde5fd7d2208444d9d697d752',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'f71c16462b3150068c887297858b3c28d68ee588',
+            'reference' => '86fc527dc3d8f97dde5fd7d2208444d9d697d752',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
